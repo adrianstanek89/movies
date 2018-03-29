0c078edbd5be111dbde993b26e8b56e623e331d5
adrianstanek89 <31177062+adrianstanek89@users.noreply.github.com>
2018-03-29 12:24:35 +0200

    Add files via upload

---
+++ moviesearch/js/movies.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    
+    $("#searchMovies").click(function(){
+        return getMovies();
+    });
+    
+});
+
+
+function getMovies(){
+    var inMovies = $("#inputMovies").val();
+    var seMovies = $("#searchMovies").val();
+    var apiKey   = "46ba6546f1215981a70723fa97097e9b";
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey  + "&query=+" + inMovies,
+        "method": "GET",
+        "headers": {},
+        "data": "{}"
+        
+}
+    var imgMovies= "https://image.tmdb.org/t/p/w500/${path}"
+
+    if(inMovies != ''){
+        
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            
+             var header = '<div class="blue" role="alert"><h4>Movies: ' + data.results[title]  + '</h4></div>'
+             
+             
+             
+             
+             
+            $("#header").html(header);
+            
+            
+            });
+        
+    }
+    
+}
+
+
+
